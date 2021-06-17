734e487186ff3d68a7c4760b7ede8fa30a3db703
akin1997 <akibh805@gmailcom>
2021-06-17 19:26:06 +0600

    add space on count

--- components/CouterDisplay.js
+++ components/CouterDisplay.js
@@ -4,5 +4,5 @@ import { CounterContext } from "./context/CounterContext";
 export default function CounterDisplay() {
   const [count] = useContext(CounterContext);
 
-  return <span className="shadow p-2 my-6 text-4xl inline-block">{count}</span>;
+  return <span className="shadow p-3 my-6 text-4xl inline-block">{count}</span>;
 }
